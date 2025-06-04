afbd341503942fd0c1cf3ab2fd1cab51b45adf49
mjsgit870 <saputraj870@gmail.com>
2025-06-04 09:41:47 +0700

    create logic for active link

--- src/components/layout/dashboard/SidebarLinksGroup/index.tsx
+++ src/components/layout/dashboard/SidebarLinksGroup/index.tsx
@@ -1,8 +1,9 @@
 import { Box, Collapse, Group, Text, ThemeIcon, UnstyledButton } from '@mantine/core';
 import { IconChevronRight } from '@tabler/icons-react';
-import { useState } from 'react';
+import React, { useState } from 'react';
 import classes from './SidebarLinksGroup.module.css';
 import Link from 'next/link';
+import { usePathname } from 'next/navigation';
 
 interface LinksGroupProps {
   icon: React.FC<any>;
@@ -13,12 +14,16 @@ interface LinksGroupProps {
 }
 
 export function LinksGroup({ icon: Icon, label, href, initiallyOpened, links }: LinksGroupProps) {
+  const pathname = usePathname();
   const hasLinks = Array.isArray(links);
   const [opened, setOpened] = useState(initiallyOpened || false);
+
+  const isActive = (link: string) => pathname === link || pathname.startsWith(`${link}/`);
+
   const items = (hasLinks ? links : []).map((link) => (
     <Text
       component={Link}
-      className={classes.link}
+      className={`${classes.link} ${isActive(link.link) ? classes.active : ''}`}
       href={link.link}
       key={link.label}
     >
@@ -26,11 +31,18 @@ export function LinksGroup({ icon: Icon, label, href, initiallyOpened, links }: 
     </Text>
   ));
 
+  const groupIsActive = hasLinks
+    ? links?.some((link) => isActive(link.link))
+    : isActive(href || '');
+
   return (
     <>
       {hasLinks ? (
-        <>
-          <UnstyledButton onClick={() => setOpened((o) => !o)} className={classes.control}>
+        <React.Fragment>
+          <UnstyledButton 
+            onClick={() => setOpened((o) => !o)} 
+            className={`${classes.control} ${groupIsActive ? classes.active : ''}`}
+          >
             <Group justify="space-between" gap={0}>
               <Box style={{ display: 'flex', alignItems: 'center' }}>
                 <ThemeIcon variant="light" size={30}>
@@ -49,9 +61,13 @@ export function LinksGroup({ icon: Icon, label, href, initiallyOpened, links }: 
             </Group>
           </UnstyledButton>
           {hasLinks ? <Collapse in={opened}>{items}</Collapse> : null}
-        </>
+        </React.Fragment>
       ) : href ? (
-        <UnstyledButton component={Link} href={href} className={classes.control}>
+        <UnstyledButton 
+          component={Link} 
+          href={href} 
+          className={`${classes.control} ${isActive(href) ? classes.active : ''}`}
+        >
           <Box style={{ display: 'flex', alignItems: 'center' }}>
             <ThemeIcon variant="light" size={30}>
               <Icon size={18} />
