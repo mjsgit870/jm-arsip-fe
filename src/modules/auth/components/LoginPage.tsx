3b28010f5b8b5d0ed3df554b9d3a75dcaf844e1b
mjsgit870 <saputraj870@gmail.com>
2025-06-10 16:35:06 +0700

    refactor style login page

--- src/modules/auth/components/LoginPage.tsx
+++ src/modules/auth/components/LoginPage.tsx
@@ -4,7 +4,7 @@ import CheckboxInput from "@/components/shared/checkbox-input";
 import PasswordInput from "@/components/shared/password-input";
 import TextInput from "@/components/shared/text-input";
 import { zodResolver } from "@hookform/resolvers/zod";
-import { Box, Button, Flex, Image, Text, Title } from "@mantine/core";
+import { Box, Button, Flex, Image, LoadingOverlay, Text, Title } from "@mantine/core";
 import { notifications } from "@mantine/notifications";
 import { IconKey, IconUser } from "@tabler/icons-react";
 import { signIn } from "next-auth/react";
@@ -44,20 +44,27 @@ export default function LoginPage() {
   }
 
   return (
-    <Flex h="100vh">
+    <Flex h={{ base: "auto", md: "100vh" }} direction={{ base: "column", md: "row" }}>
       {/* Left Side - Login */}
-      <Box flex={5}>
+      <Box flex={5} pos="relative" mt={{ base: 80, md: 0 }}>
+        <LoadingOverlay visible={loading} loaderProps={{ type: "dots" }} />
         <Flex h="100%" justify="center" align="center" direction="column" gap="md">
           <Flex direction="column" align="center" gap="sm">
             <Image 
-              src="http://frontend-nota-dinas-mitreka.apps.ocdev.jasamarga.co.id/img/brand/logo.png" 
+              src="/logo-jasamarga.png" 
               alt="Logo Jasamarga" 
               style={{ height: 50, width: "auto" }}
             />
             <Title tt="uppercase" order={3}>Jasamarga Digital Arsip</Title>
             <Text>Silahkan Masuk</Text>
           </Flex>
-          <Flex component="form" direction="column" gap="sm" miw={400} onSubmit={handleSubmit(onSubmit)}>
+          <Flex 
+            component="form" 
+            direction="column" 
+            miw={{ base: "initial", md: 400 }} 
+            gap="sm" 
+            onSubmit={handleSubmit(onSubmit)}
+          >
             <TextInput 
               control={control} 
               name="username" 
@@ -84,7 +91,7 @@ export default function LoginPage() {
       </Box>
       
       {/* Right Side - Image */}
-      <Box flex={7} p="md" bg="blue">
+      <Box flex={7} p="md" bg="blue" visibleFrom="md">
         <Flex h="100%" justify="center" align="center">
           <Image src="/login-image.svg" alt="Login Image" h="27vw" w="auto" />
         </Flex>
